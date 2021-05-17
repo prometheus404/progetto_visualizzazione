f850930e8e363aca807dbf9f07646d86965130b1
prometheus404 <cg.riccardo@gmail.com>
2021-05-17 19:06:15 +0200

    closed line on spyder graph

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -8,27 +8,17 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
 
-#################
-#   EXAMPLE     #
-#################
-
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-
-ex = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
 @app.callback(
-        Output(component_id='cancer_pollution_graph', component_property='figure'),
         Output(component_id='spyder_graph', component_property='figure'),
+        Output(component_id='area_graph', component_property='figure'),
         Input(component_id='pollutant', component_property='value'),
         Input(component_id='year', component_property='value')
         )
 def update(input1, input2):
-    return (cps(input1, input2), 
-            sg(input1, input2))
+    return ( 
+            sg2(input1, input2),
+            area_graph(input1, input2)
+            )
 
 
 #################################
@@ -56,6 +46,7 @@ def dataset_to_monthly_mean(filename, pollutant):
     # restituisce la
     return no2.groupby(by='data', as_index=False).mean('valore')
 
+#TODO chiudere la linea
 def sg(pollutant, year):
     fig = go.Figure()
     for y in range(2014, 2019):
@@ -66,10 +57,92 @@ def sg(pollutant, year):
         no2 = dataset_to_monthly_mean('../csv/pollution_detection/qaria_'+str(y)+'.csv', pollutant)
         fig.add_trace(go.Scatterpolar(r=no2['valore'], theta=no2['data'],mode = 'lines', name=y, line_color=col))
     return fig
-    
-    
 
+#########################
+#   SPYDER GRAPH 2.0    #
+#########################
+def sg2(pollutant, year):
+    months = {
+            '01': 'gen', 
+            '02': 'feb', 
+            '03': 'mar',
+            '04': 'apr',
+            '05': 'may',
+            '06': 'jun',
+            '07': 'jul',
+            '08': 'aug',
+            '09': 'sep',
+            '10': 'oct',
+            '11': 'nov',
+            '12': 'dec'}
+    yearcolor = {}
+    tot = pd.DataFrame(columns=['mese', 'stazione_id', 'valore', 'anno'])
+    for y in range(2014, 2019):
+        if y == year:
+            yearcolor[y] = "red"
+        else:
+            yearcolor[y] = "grey"
+        df = pd.read_csv('../csv/pollution_detection/qaria_'+str(y)+'.csv', sep=';')
+        pdf = df[df['inquinante']==pollutant]
+        pdf = pdf[pdf['valore'] >= 0]
+        pdf = pdf.assign(mese = lambda x: x.data)
+        pdf.mese = pdf.mese.map(lambda x: x[5:-3])
+        print(pdf)
+        pdf = pdf.groupby(by='mese', as_index=False).mean()
+        pdf = pdf.assign(anno = lambda x: y)
+        print(pdf)
+        tot = tot.append(pdf, ignore_index=True)
+    print(tot)
+        #print(pdf)
+    fig = px.line_polar(tot,
+                        r='valore',
+                        theta='mese',
+                        color='anno',
+                        color_discrete_map=yearcolor,
+                        line_close=True)
+    return fig
 
+    
+##################
+#   AREA GRAPH   #
+##################
+max_pollutant = {
+        'NO2': 200,
+        'PM10': 50,
+        'PM25': 25,
+        'O3': 120, 
+        'CO_8h': 10,
+        'SO2' : 125,
+        'C6H6': 5
+        }
+
+#TODO togliere legenda, sfondo e anno dalle x, aggiungere trasparenza e titolo con anno
+#TODO in alternativa togliere il riempimento e mettere un secondo asse y e un istogramma basato su quell'asse
+def area_graph(pollutant, year, mode='mean'):
+    max_daily = max_pollutant[pollutant]
+    ds =pd.read_csv('../csv/pollution_detection/qaria_'+str(year)+'.csv', sep=';')
+    ds = ds[ds['inquinante'] == pollutant].dropna()[['data', 'valore']]
+    if mode == 'max':
+        ds = ds.groupby(by='data').max()
+    if mode == 'mean':
+        ds = ds.groupby(by='data').mean()
+    x = ds.index#.map(lambda i: i[-5:])
+    y = ds['valore']
+    year = ds.index[0][:4]
+    maxY = [max_daily] * x.size             #max legal level
+    warY = [max_daily+max_daily] *x.size    #warning level
+    posY = y.where(y>maxY, maxY)
+    negY = y.where(y<maxY, maxY)
+    
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=x, y=maxY, line_color='white'))
+    fig.add_trace(go.Scatter(x=x, y=negY, fill='tonexty', fillcolor='green', mode='none'))
+    fig.add_trace(go.Scatter(x=x, y=maxY, line_color='white'))
+    fig.add_trace(go.Scatter(x=x, y=posY, fill='tonexty', fillcolor='red',mode='none'))
+    fig.add_trace(go.Scatter(x=x, y=y, line_color='black'))
+
+   
+    return fig
 
 #############
 #   LAYOUT  #
@@ -88,7 +161,11 @@ app.layout = html.Div(children=[
         options=[
             {'label': 'Pm 10', 'value': 'PM10'},
             {'label': 'Pm 2.5', 'value': 'PM25'},
-            {'label': 'CO 8h mean', 'value': 'CO_8h'}
+            {'label': 'CO 8h mean', 'value': 'CO_8h'},
+            {'label': 'O3', 'value': 'O3'},
+            {'label': 'C6H6', 'value': 'C6H6'},
+            {'label': 'SO2', 'value': 'SO2'},
+            {'label': 'NO2', 'value': 'NO2'}
         ],
         value='PM10'
     ),
@@ -101,14 +178,13 @@ app.layout = html.Div(children=[
         marks={i: str(i)  for i in range(2014, 2019)},
         value=2018,
     ),
-
-    dcc.Graph(
-        id='cancer_pollution_graph',
-        figure=cps('a', 'b')
-    ), 
     dcc.Graph(
         id='spyder_graph',
-        figure=cps('a', 'b')
+        figure=sg('PM10', 2018)
+    ) , 
+    dcc.Graph(
+        id='area_graph',
+        figure=area_graph('PM10', 2018)
     ) 
 ])
 
